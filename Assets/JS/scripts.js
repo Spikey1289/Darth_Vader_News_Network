37120dd7af585f47d6e4170caa7dc9d4ad41d8ab
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-01-05 17:55:06 -0500

    added some comments

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -4,78 +4,86 @@ var voiceApiCall = 'https://api.voicerss.org/?key=68c1383670f94020b6398d1b0e3a5f
 //placeholder for the drink API call
 var drinkApiCall = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-var drinkList = [];
+//function returns 25 drink objects when called
+function fetchDrinks(){
 
-fetch(drinkApiCall)
-    .then(function (result) {
-        return result.json();
-    }).then(function(drinkData){
-        // console.log(drinkData);
+    //list to store drinks in.
+    var drinkList = [];
 
-        function getIngredients(drink) {
-            const ingredients = [];
-            let i = 1;
+    fetch(drinkApiCall)
+        .then(function (result) {
+            return result.json();
+        }).then(function(drinkData){
 
-            for (const [key, value] of Object.entries(drink)) {
-                if (key === 'strIngredient' + i) {
-                    ingredients.push(value);
-                    if (i <= 15) {
-                        i++;
+
+            //logic to grab the 15 ingredients from the data provided
+            function getIngredients(drink) {
+                const ingredients = [];
+                let i = 1;
+
+                for (const [key, value] of Object.entries(drink)) {
+                    if (key === 'strIngredient' + i) {
+                        ingredients.push(value);
+                        if (i <= 15) {
+                            i++;
+                        }
                     }
                 }
+
+                return ingredients;
             }
 
-            return ingredients;
-        }
+            //logic to grab the 15 Measurments from the data provided
+            function getMeasurements(drink) {
+                const measurements = [];
+                let i = 1;
 
-        function getMeasurements(drink) {
-            const measurements = [];
-            let i = 1;
-
-            for (const [key, value] of Object.entries(drink)) {
-                if (key === 'strMeasure' + i) {
-                    measurements.push(value);
-                    if (i <= 15) {
-                        i++;
+                for (const [key, value] of Object.entries(drink)) {
+                    if (key === 'strMeasure' + i) {
+                        measurements.push(value);
+                        if (i <= 15) {
+                            i++;
+                        }
                     }
                 }
+
+                return measurements;
             }
 
-            return measurements;
-        }
+            //logic to place both the ingredients and the corrosponding measuments in an object together
+            function createIngredientData(drink) {
+                const ingredients = getIngredients(drink);
+                const measurements = getMeasurements(drink);
 
-        function createIngredientData(drink) {
-            const ingredients = getIngredients(drink);
-            const measurements = getMeasurements(drink);
+                const ingredientObjects = [];
+                for (let i = 0; i < ingredients.length; i++) {
+                    if (!ingredients[i]) {
+                        continue;
+                    }
 
-            const ingredientObjects = [];
-            for (let i = 0; i < ingredients.length; i++) {
-                if (!ingredients[i]) {
-                    continue;
+                    const ingredientObject = {
+                        ingredient: ingredients[i],
+                        measurement: measurements[i],
+                    };
+
+                    ingredientObjects.push(ingredientObject);
                 }
-
-                const ingredientObject = {
-                    ingredient: ingredients[i],
-                    measurement: measurements[i],
-                };
-
-                ingredientObjects.push(ingredientObject);
+                return ingredientObjects;
             }
-            // console.log(ingredientObjects);
-            return ingredientObjects;
-        }
 
-
-        for (var i=0; i < drinkData.drinks.length; i++) {
-            var drinkCall = drinkData.drinks[i];
-            drinkList[i] = {
-                drinkName: drinkCall.strDrink,
-                drinkCategory: drinkCall.strCategory,
-                drinkGlass: drinkCall.strGlass,
-                drinkIngredients: createIngredientData(drinkCall),
-                drinkInstructions: drinkCall.strInstructions
+            // logic to make an object per drink from the data provided.
+            for (var i=0; i < drinkData.drinks.length; i++) {
+                var drinkCall = drinkData.drinks[i];
+                drinkList[i] = {
+                    drinkName: drinkCall.strDrink,
+                    drinkCategory: drinkCall.strCategory,
+                    drinkGlass: drinkCall.strGlass,
+                    drinkIngredients: createIngredientData(drinkCall),
+                    drinkInstructions: drinkCall.strInstructions
+                }
             }
-        }
-        // console.log(drinkList);
-        return drinkList;
-    })
+            
+            //returns the list of drink objects
+            return drinkList;
+        })
+}
